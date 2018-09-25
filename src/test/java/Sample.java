1eb57bc33562585f6a3ba8fc3e9fdc8ba850da5f
AnoopAmbunhi <AnoopAmbunhi@github.com>
2018-09-25 15:58:47 +0400

    get response body

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -24,5 +25,18 @@ public class Sample {
                 .get("/distancematrix/json")
                 .then()
                 .statusCode(200);
+
+    }
+
+    @Test
+    public void getResponseBody() {
+        Response response = given()
+                .param("units", "imperial")
+                .param("origins", "Washington,DC")
+                .param("destinations", "New+York+City,NY")
+                .param("key", "AIzaSyAFNxOzcDNEZ9coJzPc_9N-CA8Euun2fDA")
+                .when()
+                .get("/distancematrix/json");
+        System.out.println(response.body().prettyPrint());
     }
 }
